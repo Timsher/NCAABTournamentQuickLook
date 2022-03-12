89f6b69f2010de96571186eb1e687fc03f8f1141
Jon Starr <timsher@gmail.com>
2022-03-12 10:56:00 -0700

    Added Save Game Routine

--- NCAARanking.py
+++ NCAARanking.py
@@ -1,3 +1,17 @@
+# Ranks basketball teams using genetic algorithms
+import pickle
 import numpy as np
-from datetime import date
-from sportsreference.ncaab.boxscore import Boxscores
+import pandas as pd
+
+
+def main():
+    with open('data/gamelist_20220123.pkl', 'rb') as fh:
+        game_list = pickle.load(fh)
+
+    with open("data/teamlist_20220123.pkl", 'rb') as fh:
+        teams = pickle.load(fh)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ Utils/SaveGames.py
@@ -0,0 +1,29 @@
+from datetime import date
+import pickle
+
+from sportsreference.ncaab.boxscore import Boxscores
+from sportsreference.ncaab.teams import Teams
+
+
+def main():
+    min_date = date(2021, 11, 9)
+    max_date = date(2022, 1, 22)
+
+    print("Fetching Games")
+    boxs = Boxscores(min_date, max_date)
+
+    print("Writing Games")
+    with open('gamelist_20220123.pkl', 'wb') as fh:
+        pickle.dump(boxs.games, fh)
+
+    print("Fetching Teams")
+    teams = Teams()
+
+    print("Writing Teams")
+    with open('teamlist_20220123.pkl', 'wb') as fh:
+        pickle.dump(teams.games, fh)
+
+
+
+if __name__ == '__main__':
+    main()
