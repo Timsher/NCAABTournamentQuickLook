3231cda6a82877e2b4adff987eae09c319fe17fa
Jon Starr <timsher@gmail.com>
2022-03-12 11:28:06 -0700

    Fixed save game bug

--- Utils/SaveGames.py
+++ Utils/SaveGames.py
@@ -7,22 +7,21 @@ from sportsreference.ncaab.teams import Teams
 
 def main():
     min_date = date(2021, 11, 9)
-    max_date = date(2022, 1, 22)
+    max_date = date(2022, 3, 12)
 
     print("Fetching Games")
     boxs = Boxscores(min_date, max_date)
 
     print("Writing Games")
-    with open('gamelist_20220123.pkl', 'wb') as fh:
+    with open('gamelist_20220312.pkl', 'wb') as fh:
         pickle.dump(boxs.games, fh)
 
     print("Fetching Teams")
     teams = Teams()
 
     print("Writing Teams")
-    with open('teamlist_20220123.pkl', 'wb') as fh:
-        pickle.dump(teams.games, fh)
-
+    with open('teamlist_20220312.pkl', 'wb') as fh:
+        pickle.dump(teams, fh)
 
 
 if __name__ == '__main__':
