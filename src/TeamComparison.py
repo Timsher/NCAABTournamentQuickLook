9f1d66738acb2f76dfb4efcf960f7094725c7711
Jon Starr <timsher@gmail.com>
2022-03-13 00:39:54 -0700

    First Working Prototype

--- src/TeamComparison.py
+++ src/TeamComparison.py
@@ -5,6 +5,7 @@
 import pickle
 import tkinter as tk
 from tkinter import ttk
+from datetime import date
 
 import numpy as np
 
@@ -18,36 +19,59 @@ def get_games(team_name, game_list, team_list):
     team_score = []
     opponent_score = []
     location = []
+    date_list = []
 
     for day in game_list:
-        for game in day:
-            if game.home_name == team_name:
-                opponent_rank.append(team_list[game.away_abbr].simple_rating_system)
-                matchup.append(game.away_name)
-                team_score.append(game.home_score)
-                opponent_score.append(game.away_score)
+        day_str = day.split('-')
+        d_date = date(int(day_str[2]), int(day_str[0]), int(day_str[1]))
+        for game in game_list[day]:
+            if game['home_name'] == team_name:
+                try:
+                    opponent_rank.append(team_list[game['away_abbr']].simple_rating_system)
+                except ValueError:
+                    opponent_rank.append(-99.9)
+                date_list.append('({:02d}/{:02d})'.format(d_date.month, d_date.day))
+                matchup.append(game['away_name'])
+                team_score.append(game['home_score'])
+                opponent_score.append(game['away_score'])
                 location.append('vs')
-            elif game.away_name == team_name:
-                opponent_rank.append(team_list[game.home_abbr].simple_rating_system)
-                matchup.append(game.home_name)
-                team_score.append(game.away_score)
-                opponent_score.append(game.home_score)
-                location.append('home')
+            elif game['away_name'] == team_name:
+                try:
+                    opponent_rank.append(team_list[game['home_abbr']].simple_rating_system)
+                except ValueError:
+                    opponent_rank.append(-99.9)
+                date_list.append('({:02d}/{:02d})'.format(d_date.month, d_date.day))
+                matchup.append(game['home_name'])
+                team_score.append(game['away_score'])
+                opponent_score.append(game['home_score'])
+                location.append('at')
     opponent_rank = np.array(opponent_rank)
     opponent_score = np.array(opponent_score)
     matchup = np.array(matchup)
     team_score = np.array(team_score)
     location = np.array(location)
-    index = np.argsort(opponent_rank)
+    date_list = np.array(date_list)
+    index = np.argsort(-opponent_rank)
     opponent_rank = opponent_rank[index]
     opponent_score = opponent_score[index]
     matchup = matchup[index]
     team_score = team_score[index]
     location = location[index]
+    date_list = date_list[index]
     ret_strings = []
+    tags = []
     for i in range(0, opponent_rank.size):
-        ret_strings.append("{:.1f} {:s} {:s} {:d}-{:d}".format(opponent_rank[i], location[i], matchup[i],
-                                                               team_score[i], opponent_score[i]))
+        try:
+            ret_strings.append("{:s} {:.1f} {:s} {:s} {:d}-{:d}\n".format(date_list[i], opponent_rank[i], location[i],
+                                                                            matchup[i], team_score[i],
+                                                                            opponent_score[i]))
+            if(team_score[i] > opponent_score[i]):
+                tags.append('win')
+            else:
+                tags.append('loss')
+        except TypeError:
+            pass
+    return ret_strings, tags
 
 
 def main():
@@ -59,6 +83,22 @@ def main():
 
     with open(TEAMFILE, 'rb') as fh:
         team_list = pickle.load(fh)
+
+    def update_left_textbox(*args):
+        txt_message_left, tags = get_games(team_name_left.get(), game_list, team_list)
+        txtbox_left.configure(state='normal')
+        txtbox_left.delete(1.0, 'end')
+        for i in range(0, len(txt_message_left)):
+            txtbox_left.insert('end', txt_message_left[i], tags[i])
+        txtbox_left.configure(state='disabled')
+
+    def update_right_textbox(*args):
+        txt_message_right, tags = get_games(team_name_right.get(), game_list, team_list)
+        txtbox_right.configure(state='normal')
+        txtbox_right.delete(1.0, 'end')
+        for i in range(0, len(txt_message_right)):
+            txtbox_right.insert('end', txt_message_right[i], tags[i])
+        txtbox_right.configure(state='disabled')
 
     team_name_list = []
     for t in team_list:
@@ -72,16 +112,34 @@ def main():
     label_right.grid(column=1, row=0)
 
     team_name_left = tk.StringVar()
-    tbox_left = ttk.Combobox(win, width=40, textvariable=team_name_left)
-    tbox_left['values'] = team_name_list
-    tbox_left.grid(column=0, row=1)
-    tbox_left.current(0)
+    cbox_left = ttk.Combobox(win, width=40, textvariable=team_name_left)
+    cbox_left['values'] = team_name_list
+    cbox_left.grid(column=0, row=1)
+    cbox_left.current(0)
 
     team_name_right = tk.StringVar()
-    tbox_right = ttk.Combobox(win, width=40, textvariable=team_name_right)
-    tbox_right['values'] = team_name_list
-    tbox_right.grid(column=1, row=1)
-    tbox_right.current(1)
+    cbox_right = ttk.Combobox(win, width=40, textvariable=team_name_right)
+    cbox_right['values'] = team_name_list
+    cbox_right.grid(column=1, row=1)
+    cbox_right.current(1)
+
+    txtbox_left = tk.Text(win, height=40, width=50)
+    txtbox_left.grid(column=0, row=2)
+    txtbox_left.tag_configure('win', background='#80CBC4')
+    txtbox_left.tag_configure('loss', background='#EF9A9A')
+    txtbox_left.configure(state='disabled')
+
+    txtbox_right = tk.Text(win, height=40, width=50)
+    txtbox_right.grid(column=1, row=2)
+    txtbox_right.tag_configure('win', background='#80CBC4')
+    txtbox_right.tag_configure('loss', background='#EF9A9A')
+    txtbox_right.configure(state='disabled')
+
+    team_name_right.trace('w', update_right_textbox)
+    team_name_left.trace('w', update_left_textbox)
+
+    update_right_textbox()
+    update_left_textbox()
 
     win.mainloop()
 
